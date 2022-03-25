b173582159adbaa6f65ad125ab00b51ba5e8d6f7
Hyeji Choi <hjchoi2323@gmail.com>
2022-03-25 09:03:43 +0900

    js init

---
+++ async/callback.js
@@ -0,0 +1,6 @@
+//Javascript is synchronous.
+//Execute the code block in order after hoisitng.
+// hoisting : var, function declaration automatically go up to the top
+console.log('1');
+console.log('2');
+console.log('3');
