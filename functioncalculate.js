e1ccd033c025de75a0ffe91c4cc70eb92b6bcd4f
Hyeji Choi <hjchoi2323@gmail.com>
2022-04-04 16:13:52 +0900

    function, class init

---
+++ functioncalculate.js
@@ -0,0 +1,28 @@
+const command = {add, substract, divide, multiply, remainder}
+
+function cal(a, b){
+    switch(command)
+    return a + b;
+}
+
+switch (command) {
+    case 'add':
+        console.log('go away!');
+        return a + b;
+        break;
+    case 'Chrome':
+        console.log('love you!');
+        return a + b;
+        break;
+    case 'Firefox':
+        console.log('love you!');
+        return a + b;
+    }
+        break;
+    default:
+        console.log('same all!');
+        return a + b;
+}
+        break;
+
+}
