a30265a67df394e969d0c8f7ddeecebb398b9bcb
Hyeji Choi <hjchoi2323@gmail.com>
2022-05-23 14:13:21 +0900

    json init

---
+++ json.js
@@ -0,0 +1,28 @@
+// JSON
+// JavaScript Object Notation
+
+// 1. Object to JSON
+// stringfy(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori', 
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${this.name} can jump!`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+
+// 2. JSON to Obejct
+// parse(json)
+
