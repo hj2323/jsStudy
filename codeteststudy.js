7272c795a12540ecf95073c05a3b3359f3d44031
Hyeji Choi <73273948+hj2323@users.noreply.github.com>
2023-01-27 23:14:28 +0900

    Create codeteststudy.js

---
+++ codeteststudy.js
@@ -0,0 +1,46 @@
+
+map() 메서드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환
+
+const array1 = [1,4,9,16];
+const map1 = array1.map(x=>x*2);
+console.log(map1);
+
+
+
+[구조분해]
+function solution(dot){
+	const [num1, num2] = dot;
+	const check = num1 * num2 >0;
+	return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+}
+
+[배열 reverse]
+array.reverse()
+
+
+[스프레드 문법]
+function solution(my_string) {
+    var answer = [...my_string].reverse().join("");
+    return answer;
+}
+
+function solution(my_string) {
+    const arr = Array.from(my_string);
+    var rearr = arr.reverse();
+    var answer = arr.toString();
+    answer = answer.replaceAll(",", "");
+    return answer;
+}
+
+
+[filter]
+const solution = (n) => Array.from({ length: n }, (_, i) => i + 1) 
+			. filter(i => i % 2 !== 0)
+function solution(array, n) {
+	return array.filter(v=>v===n).length;
+}
+
+
+[sort]
+sides = sides.sort((a,b) => a-b)
+return sides[0]+sides[1] > sides[2] ? 1:2;
