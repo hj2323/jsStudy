d96d0eed05894cf5d7cdf451e477146830d5e3b9
Hyeji Choi <73273948+hj2323@users.noreply.github.com>
2023-01-31 22:56:38 +0900

    Update codeteststudy.js

--- codeteststudy.js
+++ codeteststudy.js
@@ -56,3 +56,95 @@ return sides[0]+sides[1] > sides[2] ? 1:2;
 function solution(array) {
  return array.sort((a,b) => a-b)[Math.floor( array.length / 2)];	
 }
+
+
+[정수의 자릿수 더하기]
+function solution(n) {
+    var answer = 0;
+    var n = [...n.toString()];
+    for(i=0;i<n.length;i++){
+        answer += Number(n[i]);
+    }
+    return answer;
+}
+
+function solution(n) {
+   return n
+	.toString()
+	.split("")
+	.reduce((acc, cur) => acc + Number(cur), 0);
+}
+
+
+[substring; 부분문자열]
+str1.includes(str2) ? 1:2;
+str1.indexOf(str2) !== -1 ? 1:2;
+
+
+[제곱수 판별]
+function solution(n) {
+    var measure = [];
+    var answer = 2;
+    for(i=1;i<=n;i++){
+        if(n%i == 0) measure.push(i);
+    }
+    for(i=0;i<measure.length;i++){
+        if(measure[i]*measure[i] == n) answer = 1; 
+    }
+    return answer;
+}
+
+function solution(n) {
+   return Number.isInteger(Math.sqrt(n)) ? 1:2
+}
+
+[sum of hidden number in array]
+function solution(my_string) {
+    var arr = [...my_string];
+    var numarr = [];
+    for(i=0;i<arr.length;i++){
+        if(!isNaN(arr[i])) numarr.push(arr[i]);
+    }
+    return sum = numarr.reduce((sum, cur) => sum + Number(cur), 0);
+
+}
+
+// filter & isNaN
+function solution(my_string) {
+	return my_string.split("").filter((v) => !isNaN(v)).reduce((sum,cur) => parseInt(sum) + parseInt(cur));
+}
+
+//구조식
+function solution(my_string) {
+	return my_string.match(/[0-9]/g).reduce((sum, cur) => parseInt(sum) + parseInt(cur));
+}
+
+
+[문자도 정규표현식 가능]
+function solution(my_string) {
+	return my_string.replace(/[aeiou]/g, '');
+}
+
+
+[대문자와 소문자]
+function solution(my_string) {
+    //toUpperCase() toLowerCase()
+    var arr = my_string.split("");
+    for(i=0;i<arr.length;i++){
+        arr[i] == arr[i].toUpperCase() ? arr[i] = arr[i].toLowerCase() : arr[i] = arr[i].toUpperCase();
+    }
+    return arr.join("");
+}
+
+function solution(my_string) {
+	var answer ='';
+	for(let c of my_string) // check!!
+		answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+	return answer;
+}
+****************************************
+***try to consciously use 'map'********
+****************************************
+function solution(my_string) {
+	return my_string.split('').map(n=> n === n.toUpperCase() ? n.toLowerCase() : n.toUpperCase()).join('');
+}	
